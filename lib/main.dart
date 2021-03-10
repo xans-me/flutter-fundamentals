27ab5ff0da3acfb85821907f83109885a2550a37
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2021-03-11 04:34:05 +0700

    Material Flutter Scaffold Widget & Properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:first_flutter_app/ui/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Home());
-}
+// void main() => runApp(Home());
+
+void main() => runApp(new MaterialApp(
+      home: ScaffoldExample(),
+    ));
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
+class ScaffoldExample extends StatelessWidget {
+  // Private function
+  _tapButton() => debugPrint("Tapped Button");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scaffold Title Bar"),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent.shade700,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.email),
+              onPressed: () => debugPrint("Email Tapped!")),
+          IconButton(icon: Icon(Icons.access_alarm), onPressed: _tapButton)
+        ],
+      ),
+      body: Center(
+        child: Text("Hello Again!"),
+      ),
+    );
+  }
+}
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
