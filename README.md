5e31dc87f3fc2181cdbc2d4117e08e74165c8475
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2021-03-11 07:13:59 +0700

    Add README.md

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Basic Text Widget :
 
 1. Using Text Widget on Center
 2. Using Text Direction  : rtl / ltr
+
+## Usage Sample
+
+On main Dart :
+
+  1. Hello Flutter Example for Basic Widget
+  2. Scaffold example implement Material Design and the widget component
+  3. Biz Card Example Custom Design
+
+  Note : Change uncommented/comment block to activate sample
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,23 @@ import 'package:first_flutter_app/ui/biz_card.dart';
 import 'package:first_flutter_app/ui/home.dart';
 import 'package:flutter/material.dart';
 
+/*
+  1. Hello Flutter Example for Basic Widget
+  2. Scaffold example implement Material Design and the widget component
+  3. Biz Card Example Custom Design
+
+  Note : Change uncommented/comment block to activate sample
+ */
+
+// (1)
 // void main() => runApp(Home());
 
+// (2)
 // void main() => runApp(new MaterialApp(
 //       home: ScaffoldExample(),
 //     ));
 
+// (3)
 void main() => runApp(new MaterialApp(
       home: BizCard(),
     ));
